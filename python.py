632c3c85f2c62be909157a7abba95c5498f112c9
Devfrs1 <nttforos@gmail.com>
2026-08-29 18:05:54 +0600

    Update python.py

--- python.py
+++ python.py
@@ -1247,10 +1247,10 @@ class StatusServerHandler(BaseHTTPRequestHandler):
             self.wfile.write(html.encode('utf-8'))
 
 def run_status_server():
-    server_address = ('localhost', 5000)
+    server_address = ('0.0.0.0', 5000)
     try:
         httpd = HTTPServer(server_address, StatusServerHandler)
-        logger.info("Status HTTP server successfully started on http://0.0.0.0:3000")
+        logger.info("Status HTTP server successfully started on http://0.0.0.0:443")
         httpd.serve_forever()
     except Exception as e:
         logger.error(f"Failed to start Status HTTP server: {e}")
